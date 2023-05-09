4af57e5e28e8116285a365dab7a920ca15f10230
baizhi958216 <1475289190@qq.com>
2023-05-09 13:15:47 +0800

    feat: 数据库关联user,bookmark表, 使用class-validator校验, argon2加密, 完成用户注册
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
@@ -8,7 +10,32 @@ export class AuthService {
     return { msg: 'I have signed in' };
   }
 
-  signup() {
-    return { msg: 'I have signed up' };
+  async signup(dto: AuthDto) {
+    // hash加密密码
+    const hash = await argon.hash(dto.password);
+    try {
+      // 往数据库新增user
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        /* // 选择需要返回在user的内容
+      select: {
+        id: true,
+        email: true,
+        createAt: true,
+      }, */
+      });
+      // 移除hash
+      delete user.hash;
+      // 返回user
+      return user;
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new ForbiddenException('邮箱已存在');
+      }
+      throw error;
+    }
   }
 }

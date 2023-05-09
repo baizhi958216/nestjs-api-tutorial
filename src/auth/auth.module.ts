cc8c3772db7186971e54fa986c06138fd12cacd2
baizhi958216 <1475289190@qq.com>
2023-05-09 15:46:12 +0800

    feat: 路由jwt守卫
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrateGy } from './strategy';
 
 @Module({
   imports: [JwtModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrateGy],
 })
 export class AuthModule {}

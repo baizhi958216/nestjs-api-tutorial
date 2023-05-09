96b9cd295e95e7a5c7f73d31672e60520d81d6e7
baizhi958216 <1475289190@qq.com>
2023-05-09 14:56:45 +0800

    feat: 生成jwt
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,16 @@ import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
   async signin(dto: AuthDto) {
     // 通过邮箱查找用户
     const user = await this.prisma.user.findUnique({
@@ -19,9 +25,9 @@ export class AuthService {
     const pwMatches = await argon.verify(user.hash, dto.password);
     // 密码错误抛出异常
     if (!pwMatches) throw new ForbiddenException('密码错误');
-    // 返回用户
     delete user.hash;
-    return user;
+    // 返回jwt
+    return this.signToken(user.id, user.email);
   }
 
   async signup(dto: AuthDto) {
@@ -42,14 +48,36 @@ export class AuthService {
       }, */
       });
       // 移除hash
-      delete user.hash;
-      // 返回user
-      return user;
+      // delete user.hash;
+      // 返回jwt
+      return this.signToken(user.id, user.email);
     } catch (error) {
       if (error.code === 'P2002') {
         throw new ForbiddenException('邮箱已存在');
       }
       throw error;
     }
+  }
+
+  // jwt签名
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    // 签名
+    const secret = this.config.get('JWT_SECRET');
+    // 生成token
+    const token = await this.jwt.signAsync(payload, {
+      secret: secret,
+      expiresIn: '15m',
+    });
+
+    return {
+      access_token: token,
+    };
   }
 }
